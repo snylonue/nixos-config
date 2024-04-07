ddde95a424fefb58f4584b049de827a493b8e8c7
snylonue <snylonue@gmail.com>
2024-04-07 13:45:16 +0000

    add cachix

--- home/default.nix
+++ home/default.nix
@@ -13,7 +13,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs; [ git helix fish fastfetch nil nixfmt ];
+  home.packages = with pkgs; [ git helix fish fastfetch nil nixfmt cachix ];
 
   programs = {
     fish.enable = true;
