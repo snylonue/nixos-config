1a5073fdb768b67b4b25c0310c0f3eb1e61e96a1
snylonue <snylonue@gmail.com>
2024-07-24 13:43:33 +0000

    enable xray on shinobu

--- system/shinobu.nix
+++ system/shinobu.nix
@@ -33,6 +33,71 @@ in {
         enable = true;
         settingsFile = "/usr/local/etc/tuic/config.json";
       };
+
+      xray = {
+        enable = true;
+        settings = {
+          log.loglevel = "info";
+
+          routing = {
+            domainStrategy = "IPIfNonMatch";
+            rules = [{
+              type = "field";
+              ip = [ "geoip:cn" "geoip:private" ];
+              outboundTag = "block";
+            }];
+          };
+
+          inbounds = [{
+            listen = "0.0.0.0";
+            port = 9443;
+            protocol = "vless";
+            settings = {
+              clients = [{
+                id = secrets.client_id;
+                flow = "xtls-rprx-vision";
+              }];
+              decryption = "none";
+            };
+            streamSettings = {
+              network = "tcp";
+              security = "reality";
+              realitySettings = {
+                show = false;
+                dest = "www.universityofcalifornia.edu:443";
+                xver = 0;
+                serverNames = [
+                  "www.universityofcalifornia.edu"
+                  "workingsmarter.universityofcalifornia.edu"
+                  "universityofcalifornia.edu"
+                  "firstgen.universityofcalifornia.edu"
+                  "climate.universityofcalifornia.edu"
+                ];
+                inherit (secrets) shortIds privateKey;
+              };
+            };
+            sniffing = {
+              enabled = true;
+              destOverride = [ "http" "tls" "quic" ];
+            };
+          }];
+
+          outbounds = let mkOutbound = protocol: tag: { inherit protocol tag; };
+          in [
+            (mkOutbound "freedom" "direct")
+            (mkOutbound "blackhole" "block")
+          ];
+
+          policy = {
+            levels = {
+              "0" = {
+                handshake = 2;
+                connIdle = 120;
+              };
+            };
+          };
+        };
+      };
     };
   };
 }
